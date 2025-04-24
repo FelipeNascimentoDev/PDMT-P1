c22467d9f6b80dba51973e0634e605680ba6a77c
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-04-23 22:12:06 -0300

    feat(p1): integra com a api da viacep

--- P1-PDMT/src/components/Busca.jsx
+++ P1-PDMT/src/components/Busca.jsx
@@ -1,42 +1,62 @@
 import React, { Component } from 'react'
 import { Button } from 'primereact/button'
-import { IconField } from 'primereact/iconfield'
 import { InputText } from 'primereact/inputtext'
+import axios from 'axios'
+
 export class Busca extends Component {
-    
+
     state = {
         termoDeBusca: ''
     }
 
     onTermoAlterado = (event) => {
-        console.log(event.target.value)
         this.setState({
             termoDeBusca: event.target.value
         })
     }
 
-    onFormSubmit = (event) => {
+    onFormSubmit = async (event) => {
         event.preventDefault() // evita que o envio do formulário recarregue a página web
-        this.props.onBuscaRealizada(this.state.termoDeBusca)
+
+        const { termoDeBusca } = this.state
+
+        if (!termoDeBusca || termoDeBusca.length !== 8 || isNaN(termoDeBusca)) {
+            alert('Por favor, digite um CEP válido com 8 dígitos numéricos.')
+            return
+        }
+
+        try {
+            const resposta = await axios.get(`https://viacep.com.br/ws/${termoDeBusca}/json/`)
+            const data = resposta.data
+
+            if (data.erro) {
+                alert('CEP não encontrado.')
+                return
+            }
+
+            console.log(data)
+        } catch (error) {
+            alert('Erro ao buscar o CEP. Verifique sua conexão ou tente novamente mais tarde.')
+        }
     }
 
     render() {
         return (
             <form onSubmit={this.onFormSubmit}>
-                <div
-                    className='flex flex-column'>
-
-                    <InputText 
-                    className='w-full' 
-                    placeholder={this.props.dica}
-                    maxLength={8}
-                    onChange={this.onTermoAlterado}
-                    value={this.state.termoDeBusca}/>
-                    <br/>
+                <div className='flex flex-column'>
+                    <InputText
+                        className='w-full'
+                        placeholder={this.props.dica}
+                        maxLength={8}
+                        onChange={this.onTermoAlterado}
+                        value={this.state.termoDeBusca}
+                    />
+                    <br />
                     <Button
                         className='mt-3'
                         label='OK'
-                        outlined />
+                        outlined
+                    />
                 </div>
             </form>
         )
